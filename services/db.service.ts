3f8b32e85dc5e4367a422f6935899e604ef09e26
m-tahaali <tahaali.6074+github@gmail.com>
2024-01-30 20:04:15 +0500

    Moved to Postgres URL + Added SSL Mode Secure

--- services/db.service.ts
+++ services/db.service.ts
@@ -1,7 +1,9 @@
 import dbConfig from "../configs/db.config";
 
 import { Pool, PoolClient } from "pg";
-const pool = new Pool(dbConfig);
+const pool = new Pool({
+    connectionString: dbConfig.postgresURL + "?sslmode=require",
+})
 
 // the pool will emit an error on behalf of any idle clients it contains if a backend error or network partition happens
 pool.on('error', (err: Error, _client: PoolClient) => {
