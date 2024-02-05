cce585711cc2623668861b2236a4f1dbd0520cb8
m-tahaali <tahaali.6074+github@gmail.com>
2024-02-05 19:12:04 +0500

    Added No Listing Limit

--- services/listings.db.service.ts
+++ services/listings.db.service.ts
@@ -10,13 +10,21 @@ export type Listing = {
     sellerAddress: string
 }
 
-export async function getAllListings(limit = 100, endingPoint: string | undefined = undefined): Promise<Listing[]> {
-    let resp : QueryResult<Listing>;
+export async function getAllListings(limit: number | undefined = undefined, endingPoint: string | undefined = undefined): Promise<Listing[]> {
+    let resp: QueryResult<Listing>;
     console.log(limit);
     if (endingPoint == undefined) {
-        resp = await databasePool.query<Listing>(`SELECT * FROM "Listings" ORDER BY "ListingId" DESC LIMIT $1`, [limit]);
+        if (limit == undefined) {
+            resp = await databasePool.query<Listing>(`SELECT * FROM "Listings" ORDER BY "ListingId" DESC`);
+        } else {
+            resp = await databasePool.query<Listing>(`SELECT * FROM "Listings" ORDER BY "ListingId" DESC LIMIT $1`, [limit]);
+        }
     } else {
-        resp = await databasePool.query<Listing>(`SELECT * FROM "Listings" WHERE "ListingId" < $2 ORDER BY "ListingId" DESC LIMIT $1`, [limit, endingPoint]);
+        if (limit == undefined) {
+            resp = await databasePool.query<Listing>(`SELECT * FROM "Listings" WHERE "ListingId" < $1 ORDER BY "ListingId" DESC`, [endingPoint]);
+        } else {
+            resp = await databasePool.query<Listing>(`SELECT * FROM "Listings" WHERE "ListingId" < $2 ORDER BY "ListingId" DESC LIMIT $1`, [limit, endingPoint]);
+        }
     }
     return resp.rows;
 }
@@ -34,26 +42,26 @@ export async function getListingDetailsFromId(ListingId: bigint): Promise<false 
     return resp.rows[0];
 }
 
-export async function setListingDetails(ListingId: bigint, title: string, description: string, imageUrl: string, sellerAddress: string) : Promise<boolean> {
+export async function setListingDetails(ListingId: bigint, title: string, description: string, imageUrl: string, sellerAddress: string): Promise<boolean> {
     if (ListingId <= 0) return false;
     if (!isValidAddress(sellerAddress)) return false;
     await databasePool.query(`INSERT INTO "Listings"("ListingId", "title", "description", "imageUrl", "sellerAddress") VALUES($1, $2, $3, $4, $5) ON CONFLICT("ListingId") DO UPDATE SET "title" = $2, "description" = $3, "imageUrl" = $4;`, [ListingId, title, description, imageUrl, sellerAddress]);
     return true;
 }
 
-export async function setListingTitle(ListingId: bigint, title: string) : Promise<boolean> {
+export async function setListingTitle(ListingId: bigint, title: string): Promise<boolean> {
     if (ListingId <= 0) return false;
     let resp = await databasePool.query(`UPDATE "Listings" SET "title" = $2 WHERE "ListingId" = $1 RETURNING "ListingId"`, [ListingId, title]);
     return (resp.rowCount == 0) ? false : true;
 }
 
-export async function setListingDescription(ListingId: bigint, description: string) : Promise<boolean> {
+export async function setListingDescription(ListingId: bigint, description: string): Promise<boolean> {
     if (ListingId <= 0) return false;
     let resp = await databasePool.query(`UPDATE "Listings" SET "description" = $2 WHERE "ListingId" = $1 RETURNING "ListingId"`, [ListingId, description]);
     return (resp.rowCount == 0) ? false : true;
 }
 
-export async function setListingImageUrl(ListingId: bigint, imageUrl: string) : Promise<boolean> {
+export async function setListingImageUrl(ListingId: bigint, imageUrl: string): Promise<boolean> {
     if (ListingId <= 0) return false;
     let resp = await databasePool.query(`UPDATE "Listings" SET "imageUrl" = $2 WHERE "ListingId" = $1 RETURNING "ListingId"`, [ListingId, imageUrl]);
     return (resp.rowCount == 0) ? false : true;
